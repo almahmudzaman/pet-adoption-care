cdda0219d35c9e8b93a1e175c78ba672dfc56495
saiful455555 <saifulalamredon455.com>
2026-04-07 00:54:42 +0600

    added validation and transformation to CreatePetDto

--- src/sellers/dto/create-pet.dto.ts
+++ src/sellers/dto/create-pet.dto.ts
@@ -1,9 +1,46 @@
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
 export class CreatePetDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   name: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
   age: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   species: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   breed: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
   gender: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(500)
   description: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  @IsPositive()
   adoptionFee: number;
 }
